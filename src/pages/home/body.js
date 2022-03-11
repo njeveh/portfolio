dad3c699e0219d7d87b6cb370f07809c7516d299
njeveh <njeveh@gmail.com>
2022-03-11 19:02:45 +0300

    update

--- src/pages/home/body.js
+++ src/pages/home/body.js
@@ -3,7 +3,7 @@ const Body = () => {
         <>
             <div className="page-body">
                 <div className='main-image-container col-12 col-s-12' id='main-image-container'>
-                    <img className='main-image col-12 col-s-12' src={require("../../images/fotis-fotopoulos-6sAl6aQ4OWI-unsplash.jpg")} alt="home page main image" />
+                    <img className='main-image col-12 col-s-12' src={require("../../images/fotis-fotopoulos-6sAl6aQ4OWI-unsplash.jpg")} alt="Welcome" />
                     <div className='main-image-caption col-12 col-s-12'>
                         <h1 className='main-image-heading col-12 col-s-12'>A Computer Science Undergraduate and Software Developer</h1>
                     </div>
