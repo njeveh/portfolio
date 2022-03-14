911aadeabe2f0536de36a865e238b0bd8f81965f
njeveh <njeveh@gmail.com>
2022-03-15 01:10:00 +0300

    updates on home page

--- src/pages/home/body.js
+++ src/pages/home/body.js
@@ -8,6 +8,30 @@ const Body = () => {
                         <h1 className='main-image-heading col-12 col-s-12'>A Computer Science Undergraduate and Software Developer</h1>
                     </div>
                 </div>
+                <div className="skill-banner-container col-12 col-s-12">
+                    <div className="skill-banner">
+                        <div className="skill-title">Skill</div>
+                        <img className='skill-image' src={require("../../images/devops-engineers-web-design-with-people-vector.jpg")} alt="Welcome" />
+                        <div className="skill-summary">
+                            Skill summary<br />
+                            Skill summary<br />
+                            Skill summary<br />
+                            Skill summary<br />
+                            Skill summary
+                        
+                        </div>
+                    </div>
+                    <div className="skill-banner">
+                        <div className="skill-title">Skill</div>
+                        <img className='skill-image' src={require("../../images/team-of-developers-working-on-code-free-vector.jpg")} alt="Welcome" />
+                        <div className="skill-summary">Skill summary</div>
+                    </div>
+                    <div className="skill-banner">
+                        <div className="skill-title">Skill</div>
+                        <img className='skill-image' src={require("../../images/mobil3.jpg")} alt="Welcome" />
+                        <div className="skill-summary">Skill summary</div>
+                    </div>
+                </div>
             </div>
 
         </>
